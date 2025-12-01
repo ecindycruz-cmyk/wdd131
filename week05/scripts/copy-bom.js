6ec51fcad1449039446833fc1a72c4b135b125f6
ecindycruz-cmyk <ccruzderomero@byupathway.edu>
2025-11-30 18:40:59 -0600

    I added the JavaScript localStorage

--- week05/scripts/copy-bom.js
+++ week05/scripts/copy-bom.js
@@ -2,47 +2,70 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+// 1. Declarar el array y asignarle el valor de localStorage o un array vacío
+// chaptersArray será un array de strings (capítulos)
+let chaptersArray = getChapterList() || [];
 
-
-function setupDeleteButtons() {
-    document.querySelectorAll('.delete-button').forEach(dButton => {
-        dButton.addEventListener('click', () => {
-            const li = dButton.parentNode;
-            list.removeChild(li);
-            input.focus();
-        });
-    });
-}
-
-
-setupDeleteButtons();
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
 button.addEventListener('click', () => {
     if (input.value.length > 0) {
+    
+        const newChapter = input.value;
 
-        const addChapter = input.value;
+        displayList(newChapter);
 
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
+        chaptersArray.push(newChapter);
 
-        li.textContent = addChapter;
-
-        deleteButton.textContent = 'X';
-
-        deleteButton.classList.add('delete-button');
-
-
-        deleteButton.setAttribute('aria-label', `Remove ${addChapter}`);
-
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
-            input.focus();
-        });
-
-        li.append(deleteButton);
-        list.append(li);
+        setChapterList();
 
         input.value = '';
         input.focus();
     }
 });
+
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = item;
+    deleteButton.textContent = '❌'; 
+
+    deleteButton.classList.add('delete-button');
+    deleteButton.setAttribute('aria-label', `Remove ${item}`);
+
+    deleteButton.addEventListener('click', () => {
+        
+        deleteChapter(li.textContent);
+
+        
+        list.removeChild(li);
+        input.focus();
+    });
+
+    li.append(deleteButton);
+    list.append(li);
+}
+
+function setChapterList() {
+    
+    localStorage.setItem('BOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    
+    const storedList = localStorage.getItem('BOMList');
+    return JSON.parse(storedList);
+}
+
+function deleteChapter(chapter) {
+
+    const chapterName = chapter.slice(0, chapter.length - 1);
+
+    chaptersArray = chaptersArray.filter(item => item !== chapterName);
+
+    setChapterList();
+}
